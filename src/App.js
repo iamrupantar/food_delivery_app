af8ab8121cc0b19b554ed7e30e320837e307ccf7
Avalanch22 <bishald087@gmail.com>
2023-07-11 00:54:59 +0530

    i have added a cart section for our website

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import SignUp from './screens/Signup';
+import Cart from './screens/Cart';
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,7 +12,15 @@ import {
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const removeFromCart = (itemId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCartItems);
+  };
+
   return (
     <Router>
       <div > 
@@ -18,6 +28,11 @@ function App() {
           <Route exact path = "/" element={<Home/>}/>
           <Route exact path = "/login" element={<Login/>}/>
           <Route exact path = "/creatuser" element={<SignUp/>}/>
+          <Route
+            exact
+            path="/cart"
+            element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />}
+          />
         </Routes>
       </div>
        
---
+++ src/screens/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Cart = ({ cartItems, removeFromCart }) => {
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <span>${item.price}</span>
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
