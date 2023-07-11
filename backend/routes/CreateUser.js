a4b76cff783f7ee5ea4800da72f690606f04a03b
Rupantar-68 <chakrabortyr2002@gmail.com>
2023-07-11 15:52:16 +0530

    login_encription_authencation_done

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const axios = require('axios')
 const fetch = require('../middleware/fetchdetails');
-const jwtSecret = "HaHa"
+const jwtSecret = "MyNameIsRupantarChakraborty"
 
 router.post("/creatuser", [
     body('email', 'Error! ,Enter a valid email').isEmail(),
@@ -56,7 +56,7 @@ router.post("/loginuser", [
     body('email', "Enter a Valid Email").isEmail(),
     body('password', "Password cannot be blank").exists(),
 ], async (req, res) => {
-    
+    let success = false //
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -65,14 +65,26 @@ router.post("/loginuser", [
     let email = req.body.email;
     try {
         // let userData = await User.findOne(email);  //{email:email} === {email}
-        let user = await User.findOne({ email: email });
+        let userData = await User.findOne({ email: email });
         if (!userData) {
-            return res.status(400).json({error: "Try Logging in with correct credentials" });
+            return res.status(400).json({success,error: "Try Logging in with correct credentials" });
         }
-        if(req.body.password !==  userData.password){
-            return res.status(400).json({error: "Try Logging in with correct credentials" });
+        const pwdCompare = await bcrypt.compare(req.body.password,userData.password)
+        if(!pwdCompare){
+            return res.status(400).json({success,error: "Try Logging in with correct credentials" });
         }
-        return res.json({success:true });
+
+        
+        //mongo
+        const data = {
+            user: {
+                id: userData.id
+            }
+        }
+        success = true;
+        const authToken = jwt.sign(data, jwtSecret);
+        return res.json({success:true,authToken:authToken});
+
 
     } catch (error) {
         console.error(error.message)
