ec82946b652d76d02b2079ef11771a0760736147
Rupantar-68 <chakrabortyr2002@gmail.com>
2023-07-12 16:14:55 +0530

    login_logout_feature_added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
-  return (
-//old code
-//     <div className="navbar navbar-light bg-success">
-//       <nav className="navbar navbar-expand-lg navbar-light bg-success">
-//         <div className="container-fluid">
-//           <Link className="navbar-brand fs-1 fst-italic" to="/">FoodCart</Link>
-//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className='d-flex justify-content-end d-inline-block'> 
-// 
-//           <div className="collapse navbar-collapse" id="navbarNav">
-//             <ul className="navbar-nav me-auto mb-2">
-//               <li className="nav-item">
-//                 <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
-//               </li>
-//             </ul>
-//           </div>
-//           <div>
-//             <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
-//             <Link className="btn bg-white text-success mx-1" to="/creatuser">SignUp</Link>
-//           </div>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem("authToken");
+        navigate("/login");
+    }
 
-<div>
+    return (
+        <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky" style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
                 <div className="container-fluid">
                     <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
@@ -41,16 +21,34 @@ export default function Navbar() {
                             <li className="nav-item">
                                 <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/">Home</Link>
                             </li>
+                            {(localStorage.getItem("authToken")) ?
+                                <li className="nav-item">
+                                    <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/">My Orders</Link>
+                                </li>
+                                : ""
+                            }
                         </ul>
-                            <form className="d-flex">
-                                <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
-                                <Link className="btn bg-white text-success mx-1" to="/creatuser">Signup</Link>
-                            </form> 
-                            <div>
+                        <div>
+                            {(!localStorage.getItem("authToken")) ?
+                                <form className="d-flex">
+                                    <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
+                                    <Link className="btn bg-white text-success mx-1" to="/creatuser">Signup</Link>
+                                </form>
+                                :
+
+                                <div>
+                                    <div className='btn bg-white text-success mx-1'>
+                                        My Cart
+                                    </div>
+                                    <div className='btn bg-white text-success mx-1' onClick={handleLogout}>
+                                        LogOut
+                                    </div>
+                                </div>
+                            }
+                        </div>
                     </div>
-                </div>
                 </div>
             </nav>
         </div>
-  )
+    )
 }
